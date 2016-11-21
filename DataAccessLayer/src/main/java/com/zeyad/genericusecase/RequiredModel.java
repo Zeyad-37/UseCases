191cf42b67b84ed3506dc0d2a8f8095bf3692bf8
zeyadg <zeyad.gasser@grability.com>
2016-11-21 18:16:16 -0500

    started file and prefs use case tests, code clean up

--- DataAccessLayer/src/main/java/com/zeyad/genericusecase/RequiredModel.java
+++ DataAccessLayer/src/main/java/com/zeyad/genericusecase/RequiredModel.java
@@ -5,7 +5,7 @@ import io.realm.RealmObject;
 /**
  * DO NOT DELETE! Essential for compilation!
  */
-public class SampleModel extends RealmObject {
+public class RequiredModel extends RealmObject {
 
     boolean value;
 }
---
+++ DataAccessLayer/src/test/java/com/zeyad/genericusecase/domain/interactor/FileUseCaseTest.java
@@ -0,0 +1,11 @@
+package com.zeyad.genericusecase.domain.interactor;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+/**
+ * @author zeyad on 11/21/16.
+ */
+@RunWith(JUnit4.class)
+public class FileUseCaseTest {
+}
