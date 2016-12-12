ca0673512883f734eb22ce14d6fefb11b25c7a10
zeyadg <zeyad.gasser@grability.com>
2016-12-12 17:46:43 -0500

    started auto map annotation processor

--- app/src/main/java/com/zeyad/generic/usecase/dataaccesslayer/models/ui/UserModel.java
+++ app/src/main/java/com/zeyad/generic/usecase/dataaccesslayer/models/ui/UserModel.java
@@ -5,7 +5,6 @@ import com.google.gson.annotations.SerializedName;
 /**
  * @author zeyad on 12/1/16.
  */
-
 public class UserModel {
 
     public static final String ID = "id", COVER_URL = "cover_url", FULL_NAME = "full_name",
@@ -27,51 +26,51 @@ public class UserModel {
     public UserModel() {
     }
 
-    public void setId(int id) {
-        this.id = id;
-    }
-
     public int getId() {
         return this.id;
     }
 
-    public void setCover_url(String cover_url) {
-        this.cover_url = cover_url;
+    public void setId(int id) {
+        this.id = id;
     }
 
     public String getCover_url() {
         return this.cover_url;
     }
 
-    public void setFull_name(String full_name) {
-        this.full_name = full_name;
+    public void setCover_url(String cover_url) {
+        this.cover_url = cover_url;
     }
 
     public String getFull_name() {
         return this.full_name;
     }
 
-    public void setDescription(String description) {
-        this.description = description;
+    public void setFull_name(String full_name) {
+        this.full_name = full_name;
     }
 
     public String getDescription() {
         return this.description;
     }
 
-    public void setFollowers(int followers) {
-        this.followers = followers;
+    public void setDescription(String description) {
+        this.description = description;
     }
 
     public int getFollowers() {
         return this.followers;
     }
 
-    public void setEmail(String email) {
-        this.email = email;
+    public void setFollowers(int followers) {
+        this.followers = followers;
     }
 
     public String getEmail() {
         return this.email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
     }
 }
