c4556533fb64f56400de547ee7fb3cc72c524d92
zeyadg <zeyad.gasser@grability.com>
2016-12-13 18:35:22 -0500

    started code gen, code clean up

--- accessLayer/src/main/java/com/zeyad/usecases/data/mappers/DaoMapperUtil.java
+++
@@ -1,7 +0,0 @@
-package com.zeyad.usecases.data.mappers;
-
-public abstract class DaoMapperUtil implements IDaoMapperUtil {
-
-    @Override
-    public abstract IDaoMapper getDataMapper(Class dataClass);
-}
